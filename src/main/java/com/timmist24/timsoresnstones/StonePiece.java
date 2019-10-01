c30c26de752cb75f8b06d1aa9337a519195d0c81
timmist <xtimmist24x@gmail.com>
2019-10-01 21:51:28 +0100

    items children test3

--- src/main/java/com/timmist24/timsoresnstones/StonePiece.java
+++ src/main/java/com/timmist24/timsoresnstones/StonePiece.java
@@ -2,8 +2,16 @@ package com.timmist24.timsoresnstones;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.apache.commons.lang3.ObjectUtils;
 
 public class StonePiece extends Item {
+    static final CreativeTabs timsResorces = (new CreativeTabs() {
+        @Override
+        public ItemStack getTabIconItem() {
+            return ItemStack(Null);
+        }
+    })
     private final String name;
 
     public StonePiece(String name){
