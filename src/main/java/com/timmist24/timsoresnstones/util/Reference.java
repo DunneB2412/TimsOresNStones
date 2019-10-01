dca274dc559102fbc93cbc365e104b1af42d930d
timmist <xtimmist24x@gmail.com>
2019-10-01 22:11:40 +0100

    reference

---
+++ src/main/java/com/timmist24/timsoresnstones/util/Reference.java
@@ -0,0 +1,9 @@
+package com.timmist24.timsoresnstones.util;
+
+import scala.tools.nsc.settings.Final;
+
+public class Reference {
+    public static final String MOD_ID = "tosm";
+    public static final String  NAME = "Tim's ores and stones";
+    public static final String VERSION = "0.0";
+}
