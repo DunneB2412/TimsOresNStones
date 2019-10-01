e129d233f8c632075ae9093f4d00a7ec6c2f48dc
timmist <xtimmist24x@gmail.com>
2019-10-01 22:51:54 +0100

    re organising

--- src/main/java/com/timmist24/timsoresnstones/TimsOresNStonesMain.java
+++ src/main/java/com/timmist24/timsoresnstones/TimsOresNStonesMain.java
@@ -2,13 +2,11 @@ package com.timmist24.timsoresnstones;
 
 import com.timmist24.timsoresnstones.proxy.CommonProxy;
 import com.timmist24.timsoresnstones.util.References;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -30,10 +28,12 @@ public class TimsOresNStonesMain
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-        Item test = new Item();
-        test.setUnlocalizedName("test");
-        test.setCreativeTab(CreativeTabs.DECORATIONS);
+
+    }
+
+    @EventHandler
+    public void PostInit(FMLPostInitializationEvent event)
+    {
+
     }
 }
