06db549329290fc6eb66fbcb35286abd481389b7
timmist <xtimmist24x@gmail.com>
2019-10-06 21:06:02 +0100

    item register (registry handler)4

--- src/main/java/com/timmist24/timsoresnstones/init/ModItems.java
+++ src/main/java/com/timmist24/timsoresnstones/init/ModItems.java
@@ -9,5 +9,5 @@ import java.util.List;
 public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
-    public static final Item TEST_ITEM = new ItemBase("test item");
+    public static final Item TEST_ITEM = new ItemBase("test_item");
 }
