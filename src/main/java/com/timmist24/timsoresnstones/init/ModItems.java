ba944e33a0eb3c3111249a8e936a8ca4fad21c29
timmist <xtimmist24x@gmail.com>
2019-10-07 22:32:42 +0100

    testing iron ore temp

--- src/main/java/com/timmist24/timsoresnstones/init/ModItems.java
+++ src/main/java/com/timmist24/timsoresnstones/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.timmist24.timsoresnstones.init;
 
 import com.timmist24.timsoresnstones.items.ItemBlock;
+import com.timmist24.timsoresnstones.items.ItemOrePiece;
 import com.timmist24.timsoresnstones.items.ItemResorces;
 import com.timmist24.timsoresnstones.items.ItemStonePiece;
 import net.minecraft.item.Item;
@@ -12,10 +13,11 @@ public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
 
-    public static final Item RESORCE_ICON = new ItemResorces("resorce_icon");
     public static final Item BLOCK_ICON = new ItemBlock("block_icon");
 
     public static final Item STONE_PIECE = new ItemStonePiece("stone_piece");
+
+    public static final Item IRON_ORE_PIECE = new ItemOrePiece("iron_ore_piece", "iron");
 
 
 }
