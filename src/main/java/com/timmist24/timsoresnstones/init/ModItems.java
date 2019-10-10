f6ea035384e48cb6d6f3be59588cfda3227042ff
timmist24 <xtimmist24#@gmail.com>
2019-10-10 22:06:26 +0100

    block test/ fixing attempt 2 (somewhat funchonal)

--- src/main/java/com/timmist24/timsoresnstones/init/ModItems.java
+++ src/main/java/com/timmist24/timsoresnstones/init/ModItems.java
@@ -10,9 +10,6 @@ public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
 
-    //public static final Item BLOCK_ICON = new ItemBase("block_icon");
-    //public static final Item IRON_TEST = new ItemBlock("iron_test");
-
     public static final Item STONE_PIECE = new ItemStonePiece("stone_piece");
     public static final Item IRON_ORE_PIECE = new ItemOrePiece("iron_ore_piece", "iron");
 
