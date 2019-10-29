ed72f7359fce2c0944b479361041146b291dffcb
timmist24 <41476038+timmist24@users.noreply.github.com>
2019-10-29 20:34:09 +0000

    Update OrePiece.java

--- src/main/java/com/timmist24/timsoresnstones/items/materials/ore/OrePiece.java
+++ src/main/java/com/timmist24/timsoresnstones/items/materials/ore/OrePiece.java
@@ -6,4 +6,17 @@ public class OrePiece extends ItemTonsWithMinerals {
     public OrePiece(String itemName, Mineral[] composition) {
         super(itemName, composition, "item.ore_piece");
     }
+    
+    public String getItemStackDisplayName(ItemStack stack)
+    {
+        String s = ("" + I18n.translateToLocal(this.getUnlocalizedName() + ".name")).trim();
+        String s1 = EntityList.getTranslationName(getNamedIdFrom(stack));
+
+        if (s1 != null)
+        {
+            s = s + " " + I18n.translateToLocal("entity." + s1 + ".name");
+        }
+
+        return s;
+    }
 }
